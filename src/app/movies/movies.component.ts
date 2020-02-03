f9b651a370759ea7e4ec3ef582f119085d80b62d
Deniz Can DİNÇ <ddeniz1947@gmail.com>
2020-02-04 01:46:36 +0300

    Lesson 3:Component Styles or styleUrls

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -3,6 +3,12 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'movies', // basına nokta koyulursa <div class="movies"> seklinde kullanılır> // #(hashtag) koyulursa id olarak cagrılması gerekir <div id="movies">
     templateUrl: 'movies.component.html',
+    styles:[`
+    h2{
+        color:blue;
+    }
+  
+    `]
     
 
 })
