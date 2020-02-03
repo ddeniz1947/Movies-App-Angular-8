7b9f1ea44d562f562f664b64835ec000f94621a2
Deniz Can DİNÇ <ddeniz1947@gmail.com>
2020-02-04 01:09:20 +0300

    Lesson 1:Generating Component

---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'movies', // basına nokta koyulursa <div class="movies"> seklinde kullanılır> // #(hashtag) koyulursa id olarak cagrılması gerekir <div id="movies">
+    templateUrl: 'movies.component.html',
+    
+
+})
+export class MoviesComponent{
+
+}
