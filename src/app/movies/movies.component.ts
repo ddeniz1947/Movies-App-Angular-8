caecd649750b65bb36caea7f2be94976cc98775d
Deniz Can DİNÇ <ddeniz1947@gmail.com>
2020-02-04 02:07:44 +0300

    Lesson4: Template ile çalışmalar

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { Movie } from "../movie";
 @Component({
-    selector: 'movies', // basına nokta koyulursa <div class="movies"> seklinde kullanılır> // #(hashtag) koyulursa id olarak cagrılması gerekir <div id="movies">
-    templateUrl: 'movies.component.html',
-    styles:[`
-    h2{
-        color:blue;
-    }
-  
-    `]
-    
-
+  selector: "movies", // basına nokta koyulursa <div class="movies"> seklinde kullanılır> // #(hashtag) koyulursa id olarak cagrılması gerekir <div id="movies">
+  templateUrl: "movies.component.html"
 })
-export class MoviesComponent{
+export class MoviesComponent {
+  title = "Movies List";
+  movie: Movie = {
+    id: 1,
+    name: "Movie Name"
+  };
 
+
+
+  getTitle() {
+    return this.title;
+  }
 }
