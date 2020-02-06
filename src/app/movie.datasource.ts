4a451b8aa96e97689841c4011f87628f4d0bc690
Deniz Can DİNÇ <ddeniz1947@gmail.com>
2020-02-06 22:11:46 +0300

    Lesson5

---
+++ src/app/movie.datasource.ts
@@ -0,0 +1,7 @@
+import { Movie } from "./movie";
+
+export const Movies: Movie[] = [
+  { id: 1, name: "Movie 1" },
+  { id: 2, name: "Movie 2" },
+  { id: 3, name: "Movie 3" }
+];
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,19 +1,10 @@
 import { Component } from "@angular/core";
-import { Movie } from "../movie";
+import { Movies } from "../movie.datasource";
 @Component({
   selector: "movies", // basına nokta koyulursa <div class="movies"> seklinde kullanılır> // #(hashtag) koyulursa id olarak cagrılması gerekir <div id="movies">
   templateUrl: "movies.component.html"
 })
 export class MoviesComponent {
   title = "Movies List";
-  movie: Movie = {
-    id: 1,
-    name: "Movie Name"
-  };
-
-
-
-  getTitle() {
-    return this.title;
-  }
+  movies = Movies;
 }
