fdd62c527d563a8a6249b297ca23ed3b9f258ae0
Deniz Can DİNÇ <ddeniz1947@gmail.com>
2020-02-09 01:08:03 +0300

    Leson 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
 import { MovieComponentComponent } from './movie-component/movie-component.component';
-
+import {FormsModule} from '@angular/forms';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 @NgModule({
   declarations: [
     AppComponent,
     MoviesComponent,
-    MovieComponentComponent
+    MovieComponentComponent,
+    MovieDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Movies } from "../movie.datasource";
+import { Movie } from '../movie';
 @Component({
   selector: "movies", // basına nokta koyulursa <div class="movies"> seklinde kullanılır> // #(hashtag) koyulursa id olarak cagrılması gerekir <div id="movies">
   templateUrl: "movies.component.html"
@@ -7,4 +8,10 @@ import { Movies } from "../movie.datasource";
 export class MoviesComponent {
   title = "Movies List";
   movies = Movies;
+  selectedMovie : Movie;
+  onSelect(movie:Movie): void{
+    this.selectedMovie = movie;
+  }
+
 }
+
